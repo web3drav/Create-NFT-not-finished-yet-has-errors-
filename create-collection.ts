11ab6f18146c7c767940594487cbea7a0403cb65
Web3Drav <vistrostudio@icloud.com>
2025-11-08 22:57:54 +0100

    Add files via upload

---
+++ create-collection.ts
@@ -0,0 +1,67 @@
+import {
+    createNft,
+    fetchDigitalAsset,
+    mplTokenMetadata,
+ } from "@metaplex-foundation/mpl-token-metadata";
+ 
+ import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile
+ } from "@solana-developers/helpers";
+ 
+ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+
+ import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
+
+  import { keypairIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi";
+  
+  import { clusterApiUrl} from "@solana/web3.js"; 
+ async function main() {
+
+ const connection = new Connection(clusterApiUrl("devnet"));
+
+ const user = await getKeypairFromFile("id.json");
+ 
+ await airdropIfRequired(
+ connection,
+ user.publicKey,
+ 1 * LAMPORTS_PER_SOL,
+ 0.5 * LAMPORTS_PER_SOL
+ );
+ 
+ console.log("Loaded user", user.publicKey.toBase58 ())
+ 
+ const umi = createUmi(connection.rpcEndpoint);
+ umi.use(mplTokenMetadata());
+ 
+ const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+ umi.use(keypairIdentity(umiUser));
+ 
+ 
+ console.log("Set up Umi instance  for user");
+ 
+ const collctionMint = generateSigner(umi);
+ 
+ const transaction = await createNft(umi, {
+    mint: collectionMint,
+    name: "My Collection",
+    symbol: "MC",
+    uri: "https://shorturl.at/DQHkv",
+    sellerFeeBasisPoints: percentAmount(0),
+    isCollection: true,
+   });
+   await transaction.sendAndConfirm(umi);
+   
+   const createdCollectionNft = await fetchDigitalAsset(
+    umi,
+    collectionMint.publicKey
+   );
+   
+   console.log(`Created Collection! Address is ${getExplorerLink(
+    "address",
+    createdCollectionNft.mint.publicKey,
+    "devnet"
+   )}`);
+   }
+main().catch(console.error);
